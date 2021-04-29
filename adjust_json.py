57c9c0f937eae1415e2d3e87befd836777779a6e
ckxkexing <1585799755@qq.com>
2021-04-29 14:40:56 +0800

    用时12s计算出user信息，其中三项信息，除了event的内容

--- adjust_json.py
+++ adjust_json.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # @Author: ckxkexing
 # @Date:   2021-04-28 14:01:25
-# @Last Modified by:   chenkexing
-# @Last Modified time: 2021-04-28 17:05:29
+# @Last Modified by:   ckxkexing
+# @Last Modified time: 2021-04-29 13:27:30
 import json
 def main():
     
-    with open('apache_spark_commits.json', 'r') as f:
+    with open('apache_spark/apaches_spark_issues.json', 'r') as f:
         d = json.load(f)
-    with open('adjusted_apache_spark_commits.json', 'w') as f:
+    with open('apache_spark/adjusted_apache_spark_issues.json', 'w') as f:
         json.dump(d, f, indent = 2)
 
 if __name__ == '__main__':
--- extract_features.py
+++ extract_features.py
@@ -2,12 +2,12 @@
 # @Author: chenkexing
 # @Date:   2021-04-28 16:52:07
 # @Last Modified by:   ckxkexing
-# @Last Modified time: 2021-04-29 12:29:23
+# @Last Modified time: 2021-04-29 14:26:40
 
 
 import json
 import datetime
-
+import time
 class Developer(object):
 
     def __init__(self, name, login, id):
@@ -25,12 +25,22 @@ class Developer(object):
 
         # commit
         self.user_commits_count = 0
-        self.first_commit = 0
-        self.last_commit  = 0
+        self.first_commit = None
+        self.last_commit  = None
 
+        # issue
+        self.user_issue_count = 0
+        self.first_issue = None
+        self.last_issue = None
+
+        # pulls
+        self.user_pulls_count = 0
+        self.first_pull = None
+        self.last_pull = None
 
 def time_change(utc):
     # utc = "2017-07-28T08:28:47.776Z"
+
     UTC_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
     utc_time = datetime.datetime.strptime(utc, UTC_FORMAT)
     local_time = utc_time + datetime.timedelta(hours=8)
@@ -38,34 +48,28 @@ def time_change(utc):
     return local_time
 
 def main():
+    start_time = time.time()
     # 从commit中提取出所有用户，并对其进行id编号
 
     # 之前pick过一遍,先只获取author
     users = []  # {email, users}
     commit_count = {}
-
+    user_map = {}
 # 处理出所有author信息
     with open("apache_spark/apache_spark_commits.json", 'r') as f:
         json_data = json.load(f)
         for data in json_data:
-            flag = 1
-
             if data["author"] == None or data["author"] == {}:
                 continue
-            cur_login = data["author"]["login"]
-            if cur_login not in commit_count:
-                commit_count[cur_login] = 0
-            commit_count[cur_login] += 1
-            for exist_user in users:
-                if data["author"]["login"] == exist_user.login:
-                    if time_change(exist_user.first_commit) > time_change(data["commit"]["author"]["date"]):
-                        exist_user.first_commit = data["commit"]["author"]["date"]
-                    if time_change(exist_user.last_commit) < time_change(data["commit"]["author"]["date"]):
-                        exist_user.first_commit = data["commit"]["author"]["date"]
-                    exist_user.user_commits_count += 1
-                    flag = 0
-                    break
-            if flag == 1:
+            cur_user = data["author"]
+            if cur_user["login"] in user_map:
+                exist_user = users[user_map[cur_user["login"]]]
+                if time_change(exist_user.first_commit) > time_change(data["commit"]["author"]["date"]):
+                    exist_user.first_commit = data["commit"]["author"]["date"]
+                if time_change(exist_user.last_commit) < time_change(data["commit"]["author"]["date"]):
+                    exist_user.first_commit = data["commit"]["author"]["date"]
+                exist_user.user_commits_count += 1
+            else:
                 tmp = Developer( data["commit"]["author"]["name"],data["author"]["login"], data["author"]["id"])
                 tmp.first_commit = data["commit"]["author"]["date"]
                 tmp.last_commit  = data["commit"]["author"]["date"]
@@ -73,30 +77,24 @@ def main():
                 tmp.is_author = 1
                 tmp.user_commits_count = 1
                 users.append(tmp)
+                user_map[tmp.login] = len(users) - 1
 # author end
 
 # 处理所有committer信息
     with open("apache_spark/apache_spark_commits.json", 'r') as f:
         json_data = json.load(f)
         for data in json_data:
-            flag = 1
-
             if data["committer"] == None or data["committer"] == {}:
                 continue
-            cur_login = data["committer"]["login"]
-            if cur_login not in commit_count:
-                commit_count[cur_login] = 0
-            commit_count[cur_login] += 1
-            for exist_user in users:
-                if data["committer"]["login"] == exist_user.login:
-                    if time_change(exist_user.first_commit) > time_change(data["commit"]["committer"]["date"]):
-                        exist_user.first_commit = data["commit"]["committer"]["date"]
-                    if time_change(exist_user.last_commit) < time_change(data["commit"]["committer"]["date"]):
-                        exist_user.first_commit = data["commit"]["committer"]["date"]
-                    exist_user.is_committer = 1
-                    flag = 0
-                    break
-            if flag == 1:
+            cur_user = data["committer"]
+            if cur_user["login"] in user_map:
+                exist_user = users[user_map[cur_user["login"]]]
+                if time_change(exist_user.first_commit) > time_change(data["commit"]["committer"]["date"]):
+                    exist_user.first_commit = data["commit"]["committer"]["date"]
+                if time_change(exist_user.last_commit) < time_change(data["commit"]["committer"]["date"]):
+                    exist_user.first_commit = data["commit"]["committer"]["date"]
+                exist_user.is_committer = 1
+            else:
                 tmp = Developer(data["commit"]["committer"]["name"],data["committer"]["login"], data["committer"]["id"])
                 tmp.first_commit = data["commit"]["committer"]["date"]
                 tmp.last_commit  = data["commit"]["committer"]["date"]
@@ -120,10 +118,55 @@ def main():
                     tmp.email = cp['email']
                     users.append(tmp)
 # co_patner 处理结束
-    
-    for i in range(-2,-1):
+
+# issue start
+    with open("apache_spark/apaches_spark_issues.json", 'r') as f:
+        json_data = json.load(f)
+        for data in json_data:
+            cur_user = data['user']
+            if cur_user['login'] in user_map:
+                exist_user = users[user_map[cur_user['login']]]
+                exist_user.user_issue_count += 1
+                if exist_user.first_issue==None or time_change(exist_user.first_issue) > time_change(data['created_at']):
+                    exist_user.first_issue = data['created_at']
+                if exist_user.last_issue==None or time_change(exist_user.last_issue) < time_change(data['updated_at']):
+                    exist_user.last_issue = data['updated_at']
+            else :
+                tmp = Developer(None, cur_user["login"], cur_user["id"])
+                tmp.first_issue = data['created_at']
+                tmp.last_issue  = data['updated_at']
+                tmp.user_issue_count = 1
+                users.append(tmp)
+                user_map[tmp.login] = len(users) - 1
+# issue end
+  
+
+# pull start
+# reviewer 需要['review_comments']的获取
+
+    with open("apache_spark/apache_spark_pulls.json", 'r') as f:
+        json_data = json.load(f)
+        for data in json_data:
+            cur_user = data['user']
+            if cur_user['login'] in user_map:
+                exist_user = users[user_map[cur_user['login']]]
+                exist_user.user_pulls_count += 1
+                if exist_user.first_pull==None or time_change(exist_user.first_pull) > time_change(data['created_at']):
+                    exist_user.first_pull = data['created_at']
+                if exist_user.last_pull==None or time_change(exist_user.last_pull) < time_change(data['updated_at']):
+                    exist_user.last_pull = data['updated_at']
+            else :
+                tmp = Developer(None, cur_user["login"], cur_user["id"])
+                tmp.first_pull = data['created_at']
+                tmp.last_pull  = data['updated_at']
+                tmp.user_pulls_count = 1
+                users.append(tmp)
+                user_map[tmp.login] = len(users) - 1
+# pull end
+
+    for i in range(0,2):
         print(json.dumps(users[i].__dict__ ,indent=2))
 
-    
+    print("用时:", time.time() - start_time, 's')
 if __name__ == '__main__':
     main()
